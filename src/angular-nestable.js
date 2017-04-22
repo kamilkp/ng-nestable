778bc33f97ffc6d1d1990da1f94e375f5f876c56
Furqan Aziz <furqan.aziz@arbisoft.com>
2017-04-23 01:08:06 +0500

    Fixed two way array databinding issues

--- src/angular-nestable.js
+++ src/angular-nestable.js
@@ -120,7 +120,7 @@
 
 				maxDepth        : number of levels an item can be nested (default 5)
 				group           : group ID to allow dragging between lists (default 0)
-				
+
 				listNodeName    : The HTML element to create for lists (default 'ol')
 				itemNodeName    : The HTML element to create for list items (default 'li')
 				rootClass       : The class of the root element .nestable() was used on (default 'dd')
@@ -171,10 +171,14 @@
 								if ($nestable.collapseAllOnStart) {
 									root.nestable('collapseAll');
 								}
-								root.on('change', function(){
-									$ngModel.$setViewValue(root.nestable('serialize'));
-									$scope && $scope.$root && $scope.$root.$$phase || $scope.$apply();
-								});
+                root.on('change', function(){
+                  var items = root.nestable('serialize');
+                  $ngModel.$modelValue.splice = 0;
+                  for(var i = 0, l = items.length; i<l; i++) {
+                    $ngModel.$modelValue.push(items[i]);
+                  }
+                  $scope && $scope.$root && $scope.$root.$$phase || $scope.$apply();
+                });
 							}
 						}, true);
 					};
